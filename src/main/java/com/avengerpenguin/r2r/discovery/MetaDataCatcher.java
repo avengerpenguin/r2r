6b93184c7a708283d9b0b58ac2e798a3fd52549a
Ross Fenning <fennir01@mc-s083545.local>
2015-01-08 21:16:15 +0000

    Renamed package to namespace I own to prevent confusion between this fork and the original code.

---
+++ src/main/java/com/avengerpenguin/r2r/discovery/MetaDataCatcher.java
@@ -0,0 +1,40 @@
+/*
+ *  R2R
+ *
+ *  Copyright 2010-2011 Freie Universität Berlin
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package com.avengerpenguin.r2r.discovery;
+
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.ConcurrentHashMap;
+
+import com.avengerpenguin.r2r.MetadataRepository;
+import com.avengerpenguin.r2r.*;
+
+public class MetaDataCatcher {
+	ConcurrentMap<String, MappingMetaData> metaData;
+	MetadataRepository repository;
+	
+	public MetaDataCatcher(MetadataRepository repository) {
+		this.repository = repository;
+		metaData = new ConcurrentHashMap<String, MappingMetaData>();
+	}
+	
+	public MappingMetaData getMetaDataForMapping(String mappingURI) {
+		if(!metaData.containsKey(mappingURI))
+			metaData.putIfAbsent(mappingURI, new MappingMetaData(repository.getMappingMetaData(mappingURI)));
+		return metaData.get(mappingURI);
+	}
+}
